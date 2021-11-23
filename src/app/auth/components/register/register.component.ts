74109ce898d3e0798ae8a7b421996d79263342c2
@vahe1995 <vahe.ghonakhchyan@innline.am>
2021-11-23 18:18:56 +0400

    add register template and reactive forms

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  public form: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    email: '',
+    password: '',
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
