3f0705876c30444d9632ae89cdace2b79bff4748
@vahe1995 <vahe.ghonakhchyan@innline.am>
2021-12-01 17:52:31 +0400

    top bar component second part

--- src/app/shared/modules/topBar/components/topBar/topBar.component.ts
+++ src/app/shared/modules/topBar/components/topBar/topBar.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { isLoggedInSelector } from 'src/app/auth/store/selectors';
+import { AppStateInterface } from 'src/app/shared/types/appState.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import {
+  currentUserSelector,
+  isAnonymousSelector,
+  isLoggedInSelector,
+} from '../../../../../auth/store/selectors';
 
 @Component({
   selector: 'mc-topBar',
   templateUrl: './topBar.component.html',
   styleUrls: ['./topBar.component.scss'],
 })
-export class TopBarComponent {
-  // isLoggedIn$: Observable<boolean> = this.store.pipe(
-  //   select(isLoggedInSelector)
-  // );
+export class TopBarComponent implements OnInit {
+  isLoggedIn$: Observable<boolean | null> = this.store.pipe(
+    select(isLoggedInSelector)
+  );
+  isAnonymous$: Observable<boolean> = this.store.pipe(
+    select(isAnonymousSelector)
+  );
+  currentUser$: Observable<CurrentUserInterface | null> = this.store.pipe(
+    select(currentUserSelector)
+  );
 
-  constructor(private store: Store) {}
+  constructor(private store: Store<AppStateInterface>) {}
+
+  ngOnInit(): void {}
 }
